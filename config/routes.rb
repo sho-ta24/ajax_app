dfd7d021f4ac94298b0cfb1b7f1ca631618fd67f
sho-ta24 <ushota201122@gmail.com>
2021-03-23 23:03:34 +0900

    AlaxApp準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'posts/new', to: 'posts#new'
+  root to: 'posts#index'# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'posts', to: 'posts#create'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2021_01_26_134929) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "checked"
   end
 
 end
