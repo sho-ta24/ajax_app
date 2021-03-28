e4f6678e49c884a21cbed6d9c50d7a271b6ac0ec
sho-ta24 <ushota201122@gmail.com>
2021-03-28 22:26:27 +0900

    既読機能のフロントエンド実装

--- db/migrate/20210126134929_create_posts.rb
+++ db/migrate/20210126134929_create_posts.rb
@@ -2,8 +2,8 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.text :content
-      t.timestamps
       t.boolean :checked
+      t.timestamps
     end
   end
 end
